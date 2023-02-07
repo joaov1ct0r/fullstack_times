ccf420546d9d054e2f1fdacc8ef5903ead45e866
Joao Victor <jov9051@outlook.com>
2023-02-07 20:25:56 -0300

    feat: app criado

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,33 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 export default class App {
-  public server: Express.Application;
+  public server: express.Application;
 
   constructor() {
     this.server = express();
+  }
+
+  private async middlewares() {
+    this.server.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST"],
+      })
+    );
+
+    this.server.use(cookieParser());
+
+    this.server.use(express.json());
+
+    this.server.use(express.urlencoded({ extended: true }));
+  }
+
+  private async routes() {
+    this.server.use("/test", (req, res) => {
+      return res.json({ hello: "world" });
+    });
   }
 }
