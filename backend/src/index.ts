8ffc711aaa094803bc4c6529ba97f05e4ce66de5
Joao Victor <jov9051@outlook.com>
2023-02-07 20:28:12 -0300

    refactor: index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Funcionou!");
+import App from "./app";
+
+new App().server.listen(
+  Number(process.env.SERVER_PORT),
+  String(process.env.SERVER_HOST),
+  async () => {
+    console.log("Server running!");
+  }
+);
