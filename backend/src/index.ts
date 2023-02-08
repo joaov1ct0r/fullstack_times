96dbab831b81d35d24de91edee966bdc155f660d
Joao Victor <jov9051@outlook.com>
2023-02-08 19:57:24 -0300

    feat: conexao com db estabelecida

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,31 @@
 import "dotenv/config";
 import App from "./app";
+import prismaClient from "./database/prismaClient";
 
 new App().server.listen(
   Number(process.env.SERVER_PORT),
   String(process.env.SERVER_HOST),
   async () => {
-    console.log(`Server running!`);
+    let retries: number = 5;
+
+    while (retries) {
+      try {
+        await prismaClient.$connect();
+
+        console.log("DB Connected!!");
+
+        console.log("Server running");
+
+        break;
+      } catch (error: any) {
+        console.log(error);
+
+        retries -= 1;
+
+        console.log(`retries left ${retries}!`);
+
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      }
+    }
   }
 );
