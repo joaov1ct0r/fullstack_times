20394e09e4625582d395427deaaf8701f7268f3d
Joao Victor <jov9051@outlook.com>
2023-02-08 20:47:54 -0300

    feat: repository EditTimeRepository criado

---
+++ backend/src/database/repositories/EditTimeRepository.ts
@@ -0,0 +1,24 @@
+import IEditTimeRepository from "../../interfaces/IEditTimeRepository";
+import ITime from "../../interfaces/ITime";
+import prismaClient from "../prismaClient";
+
+export default class EditTimeRepository implements IEditTimeRepository {
+  public readonly repository;
+
+  constructor() {
+    this.repository = prismaClient;
+  }
+
+  async execute(id: number, nome: string): Promise<ITime> {
+    const time = await this.repository.time.update({
+      data: {
+        nome,
+      },
+      where: {
+        id,
+      },
+    });
+
+    return time;
+  }
+}
--- backend/src/interfaces/IEditTimeRepository.ts
+++ backend/src/interfaces/IEditTimeRepository.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import ITime from "./ITime";
 
 export default interface IEditTimeRepository {
   repository: PrismaClient;
-  execute(id: number, nome: string): Promise<null>;
+  execute(id: number, nome: string): Promise<ITime>;
 }
