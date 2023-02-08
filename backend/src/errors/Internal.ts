7404665b7cbf69b63b1b2cf84a1f7436f872c742
Joao Victor <jov9051@outlook.com>
2023-02-08 20:07:01 -0300

    feat: Internal error criado

---
+++ backend/src/errors/Internal.ts
@@ -0,0 +1,10 @@
+export default class Internal extends Error {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode: number = 500) {
+    super(message);
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
