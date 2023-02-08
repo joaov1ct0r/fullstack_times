623c19e099763729dbc4be3708e0acb189c8fec6
Joao Victor <jov9051@outlook.com>
2023-02-08 20:35:53 -0300

    feat: interface ICreateTimeRepository criado

---
+++ backend/src/interfaces/ICreateTimeRepository.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+import ITime from "./ITime";
+
+export default interface ICreateTimeRepository {
+  repository: PrismaClient;
+  execute(nome: string): Promise<ITime>;
+}
