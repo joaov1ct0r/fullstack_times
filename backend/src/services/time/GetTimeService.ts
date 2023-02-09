099aa2457237db8748584a941484790caab17412
Joao Victor <jov9051@outlook.com>
2023-02-09 17:08:33 -0300

    feat: service GetTime criado

---
+++ backend/src/services/time/GetTimeService.ts
@@ -0,0 +1,20 @@
+import BadRequest from "../../errors/BadRequest";
+import IGetTimeRepository from "../../interfaces/time/IGetTimeRepository";
+
+export default class GetTimeService {
+  private readonly getTimeRepository: IGetTimeRepository;
+
+  constructor(getTimeRepository: IGetTimeRepository) {
+    this.getTimeRepository = getTimeRepository;
+  }
+
+  async execute(nome: string) {
+    const time = await this.getTimeRepository.execute(nome);
+
+    if (!time) {
+      throw new BadRequest("Time não encontrado!");
+    }
+
+    return time;
+  }
+}
