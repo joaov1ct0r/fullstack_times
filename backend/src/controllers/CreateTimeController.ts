943546a04f8d3c78e4b2c56dfcfedcf8ed141c56
Joao Victor <jov9051@outlook.com>
2023-02-09 20:25:59 -0300

    feat: controller Createtime criado

---
+++ backend/src/controllers/CreateTimeController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import ValidateTime from "../validations/validateTime";
+import BadRequest from "../errors/BadRequest";
+import GetTimeRepository from "../database/repositories/time/GetTimeRepository";
+import CreateTimeRepository from "../database/repositories/time/CreateTimeRepository";
+import CreateTimeService from "../services/time/CreateTimeService";
+
+export default class CreateTimeController {
+  async handle(req: Request, res: Response) {
+    const { error } = new ValidateTime().validateCreateTime(req.body);
+
+    if (error) {
+      throw new BadRequest(error.message);
+    }
+
+    const { nome } = req.body;
+
+    const getTimeRepository = new GetTimeRepository();
+
+    const createTimeRepository = new CreateTimeRepository();
+
+    const createTimeService = new CreateTimeService(
+      getTimeRepository,
+      createTimeRepository
+    );
+
+    try {
+      const time = await createTimeService.execute(nome);
+
+      return res
+        .status(201)
+        .json({ time, message: "Time criado com sucesso!" });
+    } catch (err: any) {
+      return res.status(err.statusCode).json({
+        error: err.message,
+        status: err.statusCode,
+      });
+    }
+  }
+}
