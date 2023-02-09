742d5d4c504ce5477e5eeed2c99fc8d7ec6f161f
Joao Victor <jov9051@outlook.com>
2023-02-09 20:40:47 -0300

    feat: deletetimecontroller route criado

--- backend/src/routes/time.ts
+++ backend/src/routes/time.ts
@@ -2,14 +2,18 @@ import express from "express";
 import Resolver from "../utils/Resolver";
 import CreateTimeController from "../controllers/time/CreateTimeController";
 import EditTimeController from "../controllers/time/EditTimeController";
+import DeleteTimeController from "../controllers/time/DeleteTimeController";
 
 const timeRouter = express.Router();
 const resolver = new Resolver();
 const createTimeController = new CreateTimeController();
 const editTimeController = new EditTimeController();
+const deleteTimeController = new DeleteTimeController();
 
 timeRouter.post("/create", resolver.handle(createTimeController.handle));
 
 timeRouter.put("/edit", resolver.handle(editTimeController.handle));
+
+timeRouter.delete("/delete", resolver.handle(deleteTimeController.handle));
 
 export default timeRouter;
