efce311b41a8c8ff0ab0e82047573fcaccc0b50b
Joao Victor <jov9051@outlook.com>
2023-02-09 20:51:25 -0300

    feat: gettimescontroller router craido

--- backend/src/routes/time.ts
+++ backend/src/routes/time.ts
@@ -4,6 +4,7 @@ import CreateTimeController from "../controllers/time/CreateTimeController";
 import EditTimeController from "../controllers/time/EditTimeController";
 import DeleteTimeController from "../controllers/time/DeleteTimeController";
 import GetTimeController from "../controllers/time/GetTimeController";
+import GetTimesController from "../controllers/time/GetTimesController";
 
 const timeRouter = express.Router();
 const resolver = new Resolver();
@@ -11,6 +12,7 @@ const createTimeController = new CreateTimeController();
 const editTimeController = new EditTimeController();
 const deleteTimeController = new DeleteTimeController();
 const getTimeController = new GetTimeController();
+const getTimesController = new GetTimesController();
 
 timeRouter.post("/create", resolver.handle(createTimeController.handle));
 
@@ -19,5 +21,7 @@ timeRouter.put("/edit", resolver.handle(editTimeController.handle));
 timeRouter.delete("/delete", resolver.handle(deleteTimeController.handle));
 
 timeRouter.get("/time", resolver.handle(getTimeController.handle));
+
+timeRouter.get("/times", resolver.handle(getTimesController.handle));
 
 export default timeRouter;
