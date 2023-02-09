ae5ecc187ecb0aae2853eb47e23114404a83db52
Joao Victor <jov9051@outlook.com>
2023-02-09 20:34:58 -0300

    feat: edittimecontroller route criado

--- backend/src/routes/time.ts
+++ backend/src/routes/time.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import Resolver from "../utils/Resolver";
 import CreateTimeController from "../controllers/CreateTimeController";
+import EditTimeController from "../controllers/EditTimeController";
 
 const timeRouter = express.Router();
 const resolver = new Resolver();
 const createTimeController = new CreateTimeController();
+const editTimeController = new EditTimeController();
 
 timeRouter.post("/create", resolver.handle(createTimeController.handle));
+
+timeRouter.put("/edit", resolver.handle(editTimeController.handle));
 
 export default timeRouter;
