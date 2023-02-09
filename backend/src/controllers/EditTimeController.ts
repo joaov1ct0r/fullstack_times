9a5a66330af8aadaada215a6a49b4b15ac6f9b7a
Joao Victor <jov9051@outlook.com>
2023-02-09 20:33:40 -0300

    feat: edittimecontroller criado

---
+++ backend/src/controllers/EditTimeController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import ValidateTime from "../validations/validateTime";
+import BadRequest from "../errors/BadRequest";
+import GetTimeRepository from "../database/repositories/time/GetTimeRepository";
+import EditTimeRepository from "../database/repositories/time/EditTimeRepository";
+import EditTimeService from "../services/time/EditTimeService";
+
+export default class EditTimeController {
+  async handle(req: Request, res: Response) {
+    const { error } = new ValidateTime().validateEditTime(req.body);
+
+    if (error) {
+      throw new BadRequest(error.message);
+    }
+
+    const { id, nome } = req.body;
+
+    const getTimeRepository = new GetTimeRepository();
+
+    const editTimeRepository = new EditTimeRepository();
+
+    const editTimeService = new EditTimeService(
+      getTimeRepository,
+      editTimeRepository
+    );
+
+    try {
+      await editTimeService.execute(Number(id), nome);
+
+      return res.status(204).send();
+    } catch (err: any) {
+      return res.status(err.statusCode).json({
+        error: err.message,
+        status: err.statusCode,
+      });
+    }
+  }
+}
