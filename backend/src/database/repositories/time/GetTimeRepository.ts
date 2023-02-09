e586351c90d1d1b3043d41fac214db020b8f7eef
Joao Victor <jov9051@outlook.com>
2023-02-08 21:01:20 -0300

    feat: repository GetTimeRepository criado

---
+++ backend/src/database/repositories/time/GetTimeRepository.ts
@@ -0,0 +1,20 @@
+import IGetTimeRepository from "../../../interfaces/time/IGetTimeRepository";
+import prismaClient from "../../prismaClient";
+
+export default class GetTimeRepository implements IGetTimeRepository {
+  public readonly repository;
+
+  constructor() {
+    this.repository = prismaClient;
+  }
+
+  async execute(nome: string) {
+    const time = await this.repository.time.delete({
+      where: {
+        nome,
+      },
+    });
+
+    return time;
+  }
+}
