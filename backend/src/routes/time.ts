2682e628d4ea8710b9334adfedd36ff1915e7f4c
Joao Victor <jov9051@outlook.com>
2023-02-09 20:47:35 -0300

    feat: getTimeControoler router criado

--- backend/src/routes/time.ts
+++ backend/src/routes/time.ts
@@ -3,17 +3,21 @@ import Resolver from "../utils/Resolver";
 import CreateTimeController from "../controllers/time/CreateTimeController";
 import EditTimeController from "../controllers/time/EditTimeController";
 import DeleteTimeController from "../controllers/time/DeleteTimeController";
+import GetTimeController from "../controllers/time/GetTimeController";
 
 const timeRouter = express.Router();
 const resolver = new Resolver();
 const createTimeController = new CreateTimeController();
 const editTimeController = new EditTimeController();
 const deleteTimeController = new DeleteTimeController();
+const getTimeController = new GetTimeController();
 
 timeRouter.post("/create", resolver.handle(createTimeController.handle));
 
 timeRouter.put("/edit", resolver.handle(editTimeController.handle));
 
 timeRouter.delete("/delete", resolver.handle(deleteTimeController.handle));
+
+timeRouter.get("/time", resolver.handle(getTimeController.handle));
 
 export default timeRouter;
