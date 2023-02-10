352321ac6f32bd1f534f86f78a0ee3b9d49f5541
Joao Victor <jov9051@outlook.com>
2023-02-09 21:16:52 -0300

    feat: deletejogadorcontroller route criado

--- backend/src/routes/jogador.ts
+++ backend/src/routes/jogador.ts
@@ -2,14 +2,21 @@ import express from "express";
 import CreateJogadorController from "../controllers/jogador/CreateJogadorController";
 import Resolver from "../utils/Resolver";
 import EditJogadorController from "../controllers/jogador/EditJogadorController";
+import DeleteJogadorController from "../controllers/jogador/DeleteJogadorController";
 
 const jogadorRouter = express.Router();
 const resolver = new Resolver();
 const createJogadorController = new CreateJogadorController();
 const editJogadorController = new EditJogadorController();
+const deleteJogadorController = new DeleteJogadorController();
 
 jogadorRouter.post("/create", resolver.handle(createJogadorController.handle));
 
 jogadorRouter.put("/edit", resolver.handle(editJogadorController.handle));
+
+jogadorRouter.delete(
+  "/delete",
+  resolver.handle(deleteJogadorController.handle)
+);
 
 export default jogadorRouter;
