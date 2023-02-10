9089602703671befe877d3d95691f72fac4c0180
Joao Victor <jov9051@outlook.com>
2023-02-09 21:08:38 -0300

    feat: editJogador router criado

--- backend/src/routes/jogador.ts
+++ backend/src/routes/jogador.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import CreateJogadorController from "../controllers/jogador/CreateJogadorController";
 import Resolver from "../utils/Resolver";
+import EditJogadorController from "../controllers/jogador/EditJogadorController";
 
 const jogadorRouter = express.Router();
 const resolver = new Resolver();
 const createJogadorController = new CreateJogadorController();
+const editJogadorController = new EditJogadorController();
 
 jogadorRouter.post("/create", resolver.handle(createJogadorController.handle));
+
+jogadorRouter.put("/edit", resolver.handle(editJogadorController.handle));
 
 export default jogadorRouter;
