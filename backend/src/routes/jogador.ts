13fd9cc1baad8c6cf3397991d9b1021397df1524
Joao Victor <jov9051@outlook.com>
2023-02-09 21:03:09 -0300

    feat: createjogador router criado

--- backend/src/routes/jogador.ts
+++ backend/src/routes/jogador.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import CreateJogadorController from "../controllers/jogador/CreateJogadorController";
+import Resolver from "../utils/Resolver";
 
 const jogadorRouter = express.Router();
+const resolver = new Resolver();
+const createJogadorController = new CreateJogadorController();
+
+jogadorRouter.post("/create", resolver.handle(createJogadorController.handle));
 
 export default jogadorRouter;
