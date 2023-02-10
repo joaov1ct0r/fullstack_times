c49f7fa8716cb0d01e71a5c5ba7a1c5471cc790e
Joao Victor <jov9051@outlook.com>
2023-02-09 21:07:57 -0300

    feat: controller editJogador criado

---
+++ backend/src/controllers/jogador/EditJogadorController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import ValidateJogador from "../../validations/validateJogador";
+import BadRequest from "../../errors/BadRequest";
+import GetJogadorRepository from "../../database/repositories/jogador/GetJogadorRepository";
+import GetTimeRepository from "../../database/repositories/time/GetTimeRepository";
+import EditJogadorRepository from "../../database/repositories/jogador/EditJogadorRepository";
+import EditJogadorService from "../../services/jogador/EditJogadorService";
+
+export default class EditJogadorController {
+  async handle(req: Request, res: Response) {
+    const { error } = new ValidateJogador().validateEditJogador(req.body);
+
+    if (error) {
+      throw new BadRequest(error.message);
+    }
+
+    const { id, nome, idade, time_id } = req.body;
+
+    const getJogadorRepository = new GetJogadorRepository();
+
+    const getTimeRepository = new GetTimeRepository();
+
+    const editJogadorRepository = new EditJogadorRepository();
+
+    const editJogadorService = new EditJogadorService(
+      getJogadorRepository,
+      getTimeRepository,
+      editJogadorRepository
+    );
+
+    try {
+      await editJogadorService.execute(
+        Number(id),
+        nome,
+        idade,
+        Number(time_id)
+      );
+
+      return res.status(204).send();
+    } catch (err: any) {
+      return res.status(err.statusCode).json({
+        error: err.message,
+        status: err.statusCode,
+      });
+    }
+  }
+}
