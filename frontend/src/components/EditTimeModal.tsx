2b3fade3b939d57d57b6bbc4bd91fad785214ad9
Joao Victor <jov9051@outlook.com>
2023-02-12 18:55:53 -0300

    feat: component EditTimeModal criado

---
+++ frontend/src/components/EditTimeModal.tsx
@@ -0,0 +1,77 @@
+import useEditTime from "../hooks/useEditTime";
+import useFormTime from "../hooks/useFormTime";
+import ICreateTime from "../interfaces/ICreateTime";
+
+export default function EditTimeModal() {
+  const { handleSubmit, register, reset } = useFormTime();
+
+  return (
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex={-1}
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">
+              Edite o nome do seu time
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <form
+            className="modal-body"
+            onSubmit={handleSubmit((data: ICreateTime) => {
+              const response = useEditTime(data);
+
+              alert(response);
+
+              reset();
+            })}
+          >
+            <span className="input-group-text mb-3" id="basic-addon2">
+              Escolha o nome do seu time:
+            </span>
+            <div className="input-group mb-3">
+              <input
+                {...register("nome", {
+                  required: {
+                    value: true,
+                    message: "Nome é obrigatorio",
+                  },
+                  pattern: {
+                    value: /[a-z][A-Z]{1,45}$/gi,
+                    message: "Nome invalido",
+                  },
+                })}
+                type="text"
+                placeholder="Nome:"
+                className="form-control"
+                required
+              ></input>
+            </div>
+
+            <button
+              type="button"
+              className="btn btn-danger mx-2"
+              data-bs-dismiss="modal"
+              onClick={() => reset()}
+            >
+              Fechar
+            </button>
+            <button type="submit" className="btn btn-success">
+              Criar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
