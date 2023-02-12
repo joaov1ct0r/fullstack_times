cafa3e53ce5e03d742aeb7ffc0370e711541b86d
Joao Victor <jov9051@outlook.com>
2023-02-12 18:55:37 -0300

    feat: hook useEditTime criado

---
+++ frontend/src/hooks/useEditTime.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import request from "../api/config";
+import ICreateTime from "../interfaces/ICreateTime";
+
+interface IEditTimeReq {
+  message: string,
+}
+
+export default function useEditTime(data: ICreateTime) {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    request.post<IEditTimeReq>("/api/time/edit", {
+      nome: data.nome
+    }).then((response) => setMessage(response.data.message))
+  }, [])
+
+  return message;
+}
