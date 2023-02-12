48dcba1e168798d97d873b5f371b68693f76bfea
Joao Victor <jov9051@outlook.com>
2023-02-12 17:57:33 -0300

    feat: List adicionado a page Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import List from "../components/List";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Header />
+      <div className="d-flex justify-content-center mt-5">
+        <List />
+      </div>
     </div>
   )
 }
