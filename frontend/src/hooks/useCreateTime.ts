3fc06db958651129aec9d5d10329a08903919cc7
Joao Victor <jov9051@outlook.com>
2023-02-12 18:34:18 -0300

    feat: hook useCreateTime criado

---
+++ frontend/src/hooks/useCreateTime.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import request from "../api/config";
+import ICreateTime from "../interfaces/ICreateTime";
+import ITime from "../interfaces/ITime";
+
+interface ICreateTimeReq {
+  message: string,
+  time: ITime
+}
+
+export default function useCreateTime(data: ICreateTime) {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    for (let i = 0; i <= 5; i++) {
+      request.post<ICreateTimeReq>("/api/time/create", {
+        nome: data.nome
+      }).then((response) => setMessage(response.data.message))
+    }
+  }, [])
+
+  return message;
+}
