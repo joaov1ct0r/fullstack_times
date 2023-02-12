7489539a405b7bcaa1933b463294bad49f013894
Joao Victor <jov9051@outlook.com>
2023-02-12 18:35:18 -0300

    feat: modal adicionado a home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import Header from "../components/Header";
 import List from "../components/List";
+import Modal from "../components/Modal";
+import ModalButton from "../components/ModalButton";
 
 export default function Home(): JSX.Element {
   return (
@@ -8,6 +10,8 @@ export default function Home(): JSX.Element {
       <div className="d-flex justify-content-center mt-5">
         <List />
       </div>
+      <ModalButton />
+      <Modal />
     </div>
   )
 }
