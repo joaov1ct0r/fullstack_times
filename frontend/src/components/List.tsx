aa1bdc0400694e4e2a892d21e4b7544cfedae329
Joao Victor <jov9051@outlook.com>
2023-02-12 17:57:12 -0300

    feat: component List criado

---
+++ frontend/src/components/List.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FiTrash, FiEdit } from "react-icons/fi";
+import useGetTimes from "../hooks/useGetTimes";
+import ITime from "../interfaces/ITime";
+
+export default function List() {
+  const times = useGetTimes();
+
+  return (
+    <table className="table bg-white w-75 mt-3 border border-dark">
+      <thead className="text-center">
+        <tr>
+          <th scope="col">Time ID</th>
+          <th scope="col">Time Nome</th>
+          <th scope="col">Nº Jogadores</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {times!.map((time: ITime) => {
+
+          return (<tr
+            className="text-center text-danger"
+            key={String(time.id)}
+            id={String(time.id)}
+          >
+            <td>
+              {time.id}
+            </td>
+            <td>{time.nome}</td>
+            <td>{time.jogador.length}</td>
+            <td>
+              <button className="border border-white bg-warning text-white">
+                <FiEdit size={20}></FiEdit>
+              </button>
+              <button
+                className="border border-white bg-danger text-white"
+              >
+                <FiTrash size={20}></FiTrash>
+              </button>
+            </td>
+          </tr>)
+        })}
+      </tbody>
+    </table>
+  );
+}
