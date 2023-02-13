d7be3aebda0a6162b6d5f2d340c22cb38d275377
Joao Victor <jov9051@outlook.com>
2023-02-13 12:22:03 -0300

    feat: hook useFormEditJogador criado

---
+++ frontend/src/hooks/useFormEditJogador.ts
@@ -0,0 +1,22 @@
+import { useForm } from "react-hook-form";
+import IEditJogadorForm from "../interfaces/IEditJogadorForm";
+import useJogador from "./useJogador";
+
+export default function useFormEditJogador() {
+  const { jogador } = useJogador();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IEditJogadorForm>({
+    defaultValues: {
+    nome: "",
+    idade: "",
+    time_id: String(jogador!.time_id),
+    id: String(jogador!.id),
+    },
+  });
+
+  return { register, handleSubmit, reset, errors };
+}
