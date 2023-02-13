f8f163ae61b8c0c0d04b856ed37211acaa408eef
Joao Victor <jov9051@outlook.com>
2023-02-13 12:25:52 -0300

    feat: page Time criado

--- frontend/src/pages/Time.tsx
+++ frontend/src/pages/Time.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import EditJogadorModal from "../components/EditJogadorModal";
 import Header from "../components/Header";
+import ListTime from "../components/ListTime";
 import SearchTime from "../components/SearchTime";
 
 export default function Time() {
@@ -9,6 +11,10 @@ export default function Time() {
       <div className="d-flex justify-content-center mt-5">
         <SearchTime />
       </div>
+      <div className="d-flex justify-content-center mt-5">
+        <ListTime />
+      </div>
+      <EditJogadorModal />
     </div>
   )
 }
