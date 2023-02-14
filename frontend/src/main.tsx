e6b13944c9498395644cde3237f3eb008900ff90
joaov1ct0r <jov9051@outlook.com>
2023-02-14 17:04:23 -0300

    feat: provider adicionado a main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { TimesProvider } from './components/TimesContext';
 import Time from './pages/Time';
 import Times from "./pages/Times";
 
@@ -17,6 +18,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <TimesProvider>
     <RouterProvider router={router} />
+    </TimesProvider>
   </React.StrictMode>,
 )
