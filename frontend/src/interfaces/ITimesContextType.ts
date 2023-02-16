fc1aa66c28eb8f55e04ddf0505217b5bf2b6f38b
joaov1ct0r <jov9051@outlook.com>
2023-02-16 17:17:02 -0300

    refactor: Time

--- frontend/src/interfaces/ITimesContextType.ts
+++ frontend/src/interfaces/ITimesContextType.ts
@@ -1,3 +1,5 @@
+import ICreateTime from "./ICreateTime";
+import IEditTimeProps from "./IEditTimeProps";
 import IJogador from "./IJogador";
 import ITime from "./ITime";
 
@@ -6,4 +8,12 @@ export default interface ITimesContextType {
     setTime: React.Dispatch<React.SetStateAction<ITime>>
     jogador: IJogador;
     setJogador: React.Dispatch<React.SetStateAction<IJogador>>
+    times: ITime[]
+    setTimes: React.Dispatch<React.SetStateAction<ITime[]>>
+    shouldFetch: boolean
+    setShouldFetch: React.Dispatch<React.SetStateAction<boolean>>
+    createTime(data: ICreateTime): void
+    editTime({ id, nome }: IEditTimeProps): void
+    deleteTime(data: string): void
+    searchTime({ nome }: ICreateTime): void
 }
