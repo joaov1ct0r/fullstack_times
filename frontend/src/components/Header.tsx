a398d3eb007bfb2e121a14a2faaa177c53708347
joaov1ct0r <jov9051@outlook.com>
2023-02-17 16:31:03 -0300

    refactor: Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import useContextStates from "../hooks/useContextStates";
 
 export default function Header(): JSX.Element {
   return (
