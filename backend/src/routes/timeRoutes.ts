5ff0db18b420b430375505f807744d7941cb7b62
joaov1ct0r <jov9051@outlook.com>
2023-02-18 12:11:35 -0300

    refactor: timeRoutes

--- backend/src/routes/timeRoutes.ts
+++ backend/src/routes/timeRoutes.ts
@@ -20,7 +20,7 @@ timeRouter.put("/edit", resolver.handle(editTimeController.handle));
 
 timeRouter.delete("/delete", resolver.handle(deleteTimeController.handle));
 
-timeRouter.get("/time", resolver.handle(getTimeController.handle));
+timeRouter.post("/time", resolver.handle(getTimeController.handle));
 
 timeRouter.get("/times", resolver.handle(getTimesController.handle));
 
