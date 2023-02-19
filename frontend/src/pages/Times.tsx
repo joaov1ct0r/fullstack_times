685d02e14419e1d0463a1801b8da5df80cd19f87
joaov1ct0r <jov9051@outlook.com>
2023-02-19 17:42:53 -0300

    refactor: Times

--- frontend/src/pages/Times.tsx
+++ frontend/src/pages/Times.tsx
@@ -1,5 +1,6 @@
 import CreateJogadorModal from "../components/CreateJogadorModal";
 import EditTimeModal from "../components/EditTimeModal";
+import ErrorHandler from "../components/ErrorHandler";
 import Header from "../components/Header";
 import List from "../components/List";
 import Modal from "../components/Modal";
@@ -9,6 +10,7 @@ export default function Times(): JSX.Element {
   return (
     <div>
       <Header />
+      <ErrorHandler />
       <div className="d-flex justify-content-center mt-5">
         <List />
         <CreateJogadorModal />
