885654a6cc399808ca9edd2f065451b6036a8e6a
joaov1ct0r <jov9051@outlook.com>
2023-02-19 17:49:03 -0300

    refactor: ErrorHandler

--- frontend/src/components/ErrorHandler.tsx
+++ frontend/src/components/ErrorHandler.tsx
@@ -4,7 +4,7 @@ export default function ErrorHandler() {
     const { errorHandler } = useContextStates();
 
     return (
-        <div className="alert alert-danger alert-dismissible fade show text-white" role="alert">
+        <div className="alert alert-danger alert-dismissible fade show" role="alert">
                 <h1>Status: {errorHandler?.status}</h1>
                 <h1>Message: {errorHandler?.message}</h1>
                 <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
