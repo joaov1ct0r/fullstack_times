302ae2544696eb7f5b5c0930ce7b0126fc4f2b58
joaov1ct0r <jov9051@outlook.com>
2023-02-19 17:42:34 -0300

    refactor: Time

--- frontend/src/pages/Time.tsx
+++ frontend/src/pages/Time.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import EditJogadorModal from "../components/EditJogadorModal";
+import ErrorHandler from "../components/ErrorHandler";
 import Header from "../components/Header";
 import ListTime from "../components/ListTime";
 import SearchTime from "../components/SearchTime";
@@ -8,6 +9,7 @@ export default function Time() {
   return (
     <div>
       <Header />
+      <ErrorHandler />
       <div className="d-flex justify-content-center mt-5">
         <SearchTime />
       </div>
