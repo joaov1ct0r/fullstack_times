abbdf03ac8f902241b8f03c68f234b58b7908162
joaov1ct0r <jov9051@outlook.com>
2023-02-20 16:31:26 -0300

    refactor: axios

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,11 +1,14 @@
-import axios from "axios";
+import axios from "axios"
+
+const apiURL = import.meta.env.VITE_API
+const apiPORT = import.meta.env.VITE_API_PORT
 
 const request = axios.create({
-  baseURL: "http://0.0.0.0:3000",
+  baseURL: `http://${apiURL}:${apiPORT}`,
   timeout: 2000,
   headers: {
     "Content-Type": "application/json",
   },
 })
 
-export default request;
+export default request
