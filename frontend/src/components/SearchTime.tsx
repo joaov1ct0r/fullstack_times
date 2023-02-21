90d0fb807a08a0c9b817db31d9bd44da4ea5e922
joaov1ct0r <jov9051@outlook.com>
2023-02-21 17:11:47 -0300

    refactor: SearchTime

--- frontend/src/components/SearchTime.tsx
+++ frontend/src/components/SearchTime.tsx
@@ -5,23 +5,26 @@ import ICreateTime from "../interfaces/ICreateTime";
 export default function SearchTime() {
   const { useFormTime, searchTime } = useContextStates();
 
-  const { handleSubmit, register, reset } = useFormTime();
+  const { handleSubmit, register, reset, errors } = useFormTime();
   return (
     <form className="d-flex" onSubmit={handleSubmit((data: ICreateTime) => {
       searchTime(data);
       reset()
     })}>
-      <input {...register("nome", {
-        required: {
-          value: true,
-          message: "Nome é obrigatorio",
-        },
-        pattern: {
-          value: /[a-z][A-Z]{1,45}$/gi,
-          message: "Nome invalido",
-        },
-      })} className="form-control me-2" type="search" placeholder="Busque um time:" aria-label="Search" required />
-
+      <div className="input-group mb-3">
+        <input {...register("nome", {
+          required: {
+            value: true,
+            message: "Nome é obrigatorio",
+          },
+          pattern: {
+            value: /[a-z][A-Z]{1,45}$/gi,
+            message: "Nome invalido",
+          },
+        })} className="form-control me-2" type="search" placeholder="Busque um time:" aria-label="Search" required />
+        <br />
+        <p className="text text-danger">{errors.nome?.message}</p>
+      </div>
       <button className="btn btn-outline-success" type="submit" >Buscar</button>
     </form>
   )
