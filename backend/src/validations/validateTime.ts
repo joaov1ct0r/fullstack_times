84ea424e0e9a26a6b3b023e505e58beea14b9d79
joaov1ct0r <jov9051@outlook.com>
2023-03-27 15:20:06 -0300

    refactor: validateTime

--- backend/src/validations/validateTime.ts
+++ backend/src/validations/validateTime.ts
@@ -1,36 +1,38 @@
 import Joi from "joi";
 
 export default class ValidateTime {
+  private schema: Joi.ObjectSchema<Object> | undefined;
+
   validateCreateTime(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       nome: Joi.string().required().min(4).max(45),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 
   validateEditTime(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       id: Joi.string().required().min(1),
       nome: Joi.string().required().min(4).max(45),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 
   validateDeleteTime(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       id: Joi.string().required().min(1),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 
   validateGetTime(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       nome: Joi.string().required().min(1).max(45),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 }
