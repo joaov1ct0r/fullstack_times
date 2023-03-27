a48623f8d8bfbef433008fd81c250d57aec323a0
joaov1ct0r <jov9051@outlook.com>
2023-03-27 15:18:42 -0300

    refactor: validateJogador

--- backend/src/validations/validateJogador.ts
+++ backend/src/validations/validateJogador.ts
@@ -1,32 +1,34 @@
 import Joi from "joi";
 
 export default class ValidateJogador {
+  private schema: Joi.ObjectSchema<Object> | undefined;
+
   validateCreateJogador(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       nome: Joi.string().required().min(4).max(45),
       idade: Joi.string().required().min(1),
       time_id: Joi.string().required().min(1),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 
   validateEditJogador(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       id: Joi.string().required().min(1),
       nome: Joi.string().required().min(4).max(45),
       idade: Joi.string().required().min(1),
       time_id: Joi.string().required().min(1),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 
   validateDeleteJogador(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       id: Joi.string().required().min(1),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 }
