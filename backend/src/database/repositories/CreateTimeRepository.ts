210bea69cfada78ebb42c30e91f4f54ebed04907
joaov1ct0r <jov9051@outlook.com>
2023-03-28 18:23:41 -0300

    refactor: CreateTimeRepository

--- backend/src/database/repositories/CreateTimeRepository.ts
+++ backend/src/database/repositories/CreateTimeRepository.ts
@@ -1,9 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import ICreateTimeRepository from "../../interfaces/ICreateTimeRepository";
 import prismaClient from "../prismaClient";
 
 export default class CreateTimeRepository implements ICreateTimeRepository {
+  private readonly prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(nome: string) {
-    const time = await prismaClient.time.create({
+    const time = await this.prismaClient.time.create({
       data: {
         nome,
       },
