8a018c56a916cca5351130c3fbef9ea5ecaeafa5
joaov1ct0r <jov9051@outlook.com>
2023-03-28 18:23:06 -0300

    refactor: CreateJogadorRepository

--- backend/src/database/repositories/CreateJogadorRepository.ts
+++ backend/src/database/repositories/CreateJogadorRepository.ts
@@ -1,11 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 import ICreateJogadorRepository from "../../interfaces/ICreateJogadorRepository";
 import prismaClient from "../prismaClient";
 
 export default class CreateJogadorRepository
   implements ICreateJogadorRepository
 {
+  private readonly prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(nome: string, idade: number, time_id: number) {
-    const jogador = await prismaClient.jogador.create({
+    const jogador = await this.prismaClient.jogador.create({
       data: {
         nome,
         idade,
