defb4968a7c141cc1ddd45c2850aebe6ddd5d061
joaov1ct0r <jov9051@outlook.com>
2023-03-28 18:24:17 -0300

    refactor: DeleteJogadorRepository

--- backend/src/database/repositories/DeleteJogadorRepository.ts
+++ backend/src/database/repositories/DeleteJogadorRepository.ts
@@ -1,11 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 import IDeleteJogadorRepository from "../../interfaces/IDeleteJogadorRepository";
 import prismaClient from "../prismaClient";
 
 export default class DeleteJogadorRepository
   implements IDeleteJogadorRepository
 {
+  private readonly prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(id: number) {
-    const jogador = await prismaClient.jogador.delete({
+    const jogador = await this.prismaClient.jogador.delete({
       where: { id },
     });
 
