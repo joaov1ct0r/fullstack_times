8930f4b1d9f9430bc17f7298c73676badbf5686a
joaov1ct0r <jov9051@outlook.com>
2023-03-28 18:25:11 -0300

    refactor: DeleteTimeRepository

--- backend/src/database/repositories/DeleteTimeRepository.ts
+++ backend/src/database/repositories/DeleteTimeRepository.ts
@@ -1,9 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import IDeleteTimeRepository from "../../interfaces/IDeleteTimeRepository";
 import prismaClient from "../prismaClient";
 
 export default class DeleteTimeRepository implements IDeleteTimeRepository {
+  private readonly prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(id: number) {
-    const time = await prismaClient.time.delete({
+    const time = await this.prismaClient.time.delete({
       where: { id },
     });
 
