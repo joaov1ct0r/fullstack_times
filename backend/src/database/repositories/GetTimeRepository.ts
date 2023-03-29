e1e9a89b49e5da061d13849e23a9ce387ddf0ec5
joaov1ct0r <jov9051@outlook.com>
2023-03-29 15:15:41 -0300

    refactor: GetTimeRepository

--- backend/src/database/repositories/GetTimeRepository.ts
+++ backend/src/database/repositories/GetTimeRepository.ts
@@ -1,16 +1,23 @@
+import { PrismaClient } from "@prisma/client";
 import IGetTimeRepository from "../../interfaces/IGetTimeRepository";
 import prismaClient from "../prismaClient";
 
 export default class GetTimeRepository implements IGetTimeRepository {
+  private readonly prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(nome?: string, id?: number) {
     const time = id
-      ? await prismaClient.time.findFirst({
+      ? await this.prismaClient.time.findFirst({
           where: { id },
           include: {
             Jogador: true,
           },
         })
-      : await prismaClient.time.findFirst({
+      : await this.prismaClient.time.findFirst({
           where: { nome },
           include: {
             Jogador: true,
