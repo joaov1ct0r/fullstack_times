f0fc00e27439a5eac1362ce1dbc90495ae970de2
joaov1ct0r <jov9051@outlook.com>
2023-03-29 15:13:09 -0300

    refactor: EditJogadorRepository

--- backend/src/database/repositories/EditJogadorRepository.ts
+++ backend/src/database/repositories/EditJogadorRepository.ts
@@ -1,9 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import IEditJogadorRepository from "../../interfaces/IEditJogadorRepository";
 import prismaClient from "../prismaClient";
 
 export default class EditJogadorRepository implements IEditJogadorRepository {
+  private readonly prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(id: number, nome: string, idade: number) {
-    const jogador = await prismaClient.jogador.update({
+    const jogador = await this.prismaClient.jogador.update({
       where: { id },
       data: {
         nome,
