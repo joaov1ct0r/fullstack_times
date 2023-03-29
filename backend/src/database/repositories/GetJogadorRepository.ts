06cc5f8e2bd18c4102441f6322271b215de3d6a0
joaov1ct0r <jov9051@outlook.com>
2023-03-29 15:14:50 -0300

    refactor: GetJogadorRepository

--- backend/src/database/repositories/GetJogadorRepository.ts
+++ backend/src/database/repositories/GetJogadorRepository.ts
@@ -1,25 +1,26 @@
 import IJogador from "../../interfaces/IJogador";
 import IGetJogadorRepository from "../../interfaces/IGetJogadorRepository";
 import prismaClient from "../prismaClient";
+import { PrismaClient } from "@prisma/client";
 
 export default class GetJogadorRepository implements IGetJogadorRepository {
+  private readonly prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(
     nome?: string | undefined,
     id?: number | undefined
   ): Promise<IJogador | null> {
     const jogador = id
-      ? await prismaClient.jogador.findUnique({
+      ? await this.prismaClient.jogador.findUnique({
           where: { id },
         })
-      : await prismaClient.jogador.findFirst({
+      : await this.prismaClient.jogador.findFirst({
           where: { nome },
         });
-
-    // let jogador;
-
-    // if (id) {
-    //   jogador = await prismaClient.jogador.find
-    // }
 
     return jogador;
   }
