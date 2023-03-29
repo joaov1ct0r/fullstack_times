434c27f3f21fdc4b0fcb619f6d5f94d32619c032
joaov1ct0r <jov9051@outlook.com>
2023-03-29 15:13:52 -0300

    refactor: EditTimeRepository

--- backend/src/database/repositories/EditTimeRepository.ts
+++ backend/src/database/repositories/EditTimeRepository.ts
@@ -1,9 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import IEditTimeRepository from "../../interfaces/IEditTimeRepository";
 import prismaClient from "../prismaClient";
 
 export default class EditTimeRepository implements IEditTimeRepository {
+  private readonly prismaClient: PrismaClient;
+  
+  constructor() {
+    this.prismaClient = prismaClient;
+  }
+
   async execute(id: number, nome: string) {
-    const time = await prismaClient.time.update({
+    const time = await this.prismaClient.time.update({
       data: {
         nome,
       },
