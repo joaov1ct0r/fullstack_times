3d4734d1d7684583de38751b41f474210e3fed65
joaov1ct0r <jov9051@outlook.com>
2023-03-31 14:13:09 -0300

    refactor: GetTimesController

--- backend/src/controllers/GetTimesController.ts
+++ backend/src/controllers/GetTimesController.ts
@@ -3,13 +3,17 @@ import GetTimesRepository from "../database/repositories/GetTimesRepository";
 import GetTimesService from "../services/GetTimesService";
 
 export default class GetTimesController {
+  private readonly getTimesRepository: GetTimesRepository;
+  private readonly getTimesService: GetTimesService;
+
+  constructor() {
+    this.getTimesRepository = new GetTimesRepository();
+    this.getTimesService = new GetTimesService(this.getTimesRepository);
+  }
+
   async handle(req: Request, res: Response) {
-    const getTimesRepository = new GetTimesRepository();
-
-    const getTimesService = new GetTimesService(getTimesRepository);
-
     try {
-      const times = await getTimesService.execute();
+      const times = await this.getTimesService.execute();
 
       return res.status(200).json({ times, message: "Success" });
     } catch (err: any) {
